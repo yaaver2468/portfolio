a9dec4aabc8ea3882c0f3112a9fb19e9307f5a60
Yaaver <placeholder@example.com>
2026-08-13 17:02:16 -0400

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: true
+	},
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
+	}
+};
